9ebbd68f11cf3e4b5bec38b367d09a6b5719bfa5
Peter Stanly <peterstanly01@outlook.com>
2024-05-29 17:48:21 -0500

    first commit

---
+++ calculator.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main(){
+    char operator;
+    double first, second;
+    printf("**************************************\n");
+    printf("CALCULATOR by Peter Stanly \n");
+    printf("Enter the Operator (+, -, *, / ) : ");
+    scanf("%c", &operator);
+
+    printf("Enter the two numbers one by one : ");
+    scanf("%lf %lf",&first,&second);
+
+switch (operator)
+{
+case '+':
+    printf("%lf + %lf = %lf", first, second,(first+second));
+    break;
+case '-':
+    printf("%lf - %lf = %lf", first, second,(first-second));
+    break;
+case '*':
+    printf("%lf * %lf = %lf", first, second,(first*second));
+    break;
+case '/':
+    if (second !=0.0)
+        printf("%lf / %lf = %lf", first, second,(first/second));
+    else
+        printf("Divide by zero situation");
+    break;
+    
+
+
+default:
+    printf("Invalid Operator");
+
+}
+
+
+    return 0;
+}
